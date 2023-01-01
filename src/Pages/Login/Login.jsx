fd3b849022e79c069745907b978ce5b02a6d3ef5
sikander-afzaal <sikander.aaa@gmail.com>
2023-01-01 21:07:12 +0500

    asd

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,12 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
+  const navigate = useNavigate();
   return (
     <div className="login">
       <h1>Trend Hacker</h1>
       <form>
         <input type="email" placeholder="Email" />
         <input type="password" placeholder="Password" />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            navigate("/tweet");
+          }}
+        >
+          Login
+        </button>
       </form>
     </div>
   );
